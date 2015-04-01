93e389b1a496fb3de43daa8d7e667309b206624e
arieonline <permana.dimasyudha@yahoo.com>
2015-04-01 18:11:02 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -155,10 +155,20 @@ cd
 apt-get -y install fail2ban;service fail2ban restart
 
 # install squid3
-apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.github.com/arieonline/autoscript/master/conf/squid3.conf"
-sed -i $MYIP2 /etc/squid3/squid.conf;
-service squid3 restart
+apt-get -y install aptitude curl
+
+# Specify our IP Server
+if [ "$IP" = "" ]; then
+        IP=$(curl -s ifconfig.me)
+fi
+
+aptitude -y install squid3
+rm -f /etc/squid3/squid.conf
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/narrundo/vishera/conf/squid.conf"
+
+# sed -i 's/#cache_dir/cache_dir/g' /etc/squid3/squid.conf
+
+sed -i "s/ipserver/$IP/g" /etc/squid3/squid.conf
 
 # install webmin
 cd
